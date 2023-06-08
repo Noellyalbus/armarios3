eb798f124d6a59ab2c82026fbf48ae20254d0ab5
Noelly Albus <115089251+Noellyalbus@users.noreply.github.com>
2023-06-07 21:48:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,259 @@
+//armario1
+function livre1(){
+    const change = document.getElementById("ar-1").style.backgroundColor ='green';    
+}
+
+function ocupado1(){
+    const change = document.getElementById("ar-1").style.backgroundColor = 'red'
+}
+
+function manutencao1(){
+    const change = document.getElementById("ar-1").style.backgroundColor = 'gold'
+}
+
+//armario 2
+function livre2(){
+    const change = document.getElementById("ar-2").style.backgroundColor ='green';    
+}
+
+function ocupado2(){
+    const change = document.getElementById("ar-2").style.backgroundColor = 'red'
+}
+
+function manutencao2(){
+    const change = document.getElementById("ar-2").style.backgroundColor = 'gold'
+}
+
+//Armario 3
+function livre3(){
+    const change = document.getElementById("ar-3").style.backgroundColor ='green';    
+}
+
+function ocupado3(){
+    const change = document.getElementById("ar-3").style.backgroundColor = 'red';
+}
+
+function manutencao3(){
+    const change = document.getElementById("ar-3").style.backgroundColor = 'gold';
+}
+
+//Armario 4
+function livre4(){
+    const change = document.getElementById("ar-4").style.backgroundColor ='green';    
+}
+
+function ocupado4(){
+    const change = document.getElementById("ar-4").style.backgroundColor = 'red';
+}
+
+function manutencao4(){
+    const change = document.getElementById("ar-4").style.backgroundColor = 'gold';
+}
+
+//Armario 5
+function livre5(){
+    const change = document.getElementById("ar-5").style.backgroundColor ='green';    
+}
+
+function ocupado5(){
+    const change = document.getElementById("ar-5").style.backgroundColor = 'red'
+}
+
+function manutencao5(){
+    const change = document.getElementById("ar-5").style.backgroundColor = 'gold'
+}
+
+//Armario 6
+function livre6(){
+    const change = document.getElementById("ar-6").style.backgroundColor ='green';    
+}
+
+function ocupado6(){
+    const change = document.getElementById("ar-6").style.backgroundColor = 'red'
+}
+
+function manutencao6(){
+    const change = document.getElementById("ar-6").style.backgroundColor = 'gold'
+}
+
+//Armario 7
+function livre7(){
+    const change = document.getElementById("ar-7").style.backgroundColor ='green';    
+}
+
+function ocupado7(){
+    const change = document.getElementById("ar-7").style.backgroundColor = 'red'
+}
+
+function manutencao7(){
+    const change = document.getElementById("ar-7").style.backgroundColor = 'gold'
+}
+
+//Armario 8
+function livre8(){
+    const change = document.getElementById("ar-8").style.backgroundColor ='green';    
+}
+
+function ocupado8(){
+    const change = document.getElementById("ar-8").style.backgroundColor = 'red'
+}
+
+function manutencao8(){
+    const change = document.getElementById("ar-8").style.backgroundColor = 'gold'
+}
+
+//Armario 9
+function livre9(){
+    const change = document.getElementById("ar-9").style.backgroundColor ='green';    
+}
+
+function ocupado9(){
+    const change = document.getElementById("ar-9").style.backgroundColor = 'red'
+}
+
+function manutencao9(){
+    const change = document.getElementById("ar-9").style.backgroundColor = 'gold'
+}
+
+//Armario 10
+function livre10(){
+    const change = document.getElementById("ar-10").style.backgroundColor ='green';    
+}
+
+function ocupado10(){
+    const change = document.getElementById("ar-10").style.backgroundColor = 'red'
+}
+
+function manutencao10(){
+    const change = document.getElementById("ar-10").style.backgroundColor = 'gold'
+}
+
+//Armario 11
+function livre11(){
+    const change = document.getElementById("ar-11").style.backgroundColor ='green';    
+}
+
+function ocupado11(){
+    const change = document.getElementById("ar-11").style.backgroundColor = 'red'
+}
+
+function manutencao11(){
+    const change = document.getElementById("ar-11").style.backgroundColor = 'gold'
+}
+
+//Armario 12
+function livre12(){
+    const change = document.getElementById("ar-12").style.backgroundColor ='green';    
+}
+
+function ocupado12(){
+    const change = document.getElementById("ar-12").style.backgroundColor = 'red'
+}
+
+function manutencao12(){
+    const change = document.getElementById("ar-12").style.backgroundColor = 'gold'
+}
+
+//Armario 13
+function livre13(){
+    const change = document.getElementById("ar-13").style.backgroundColor ='green';    
+}
+
+function ocupado13(){
+    const change = document.getElementById("ar-13").style.backgroundColor = 'red';
+}
+
+function manutencao13(){
+    const change = document.getElementById("ar-13").style.backgroundColor = 'gold';
+}
+
+//Armario 14
+function livre14(){
+    const change = document.getElementById("ar-14").style.backgroundColor ='green';    
+}
+
+function ocupado14(){
+    const change = document.getElementById("ar-14").style.backgroundColor = 'red'
+}
+
+function manutencao14(){
+    const change = document.getElementById("ar-14").style.backgroundColor = 'gold'
+}
+
+//Armario 15
+function livre15(){
+    const change = document.getElementById("ar-15").style.backgroundColor ='green';    
+}
+
+function ocupado15(){
+    const change = document.getElementById("ar-15").style.backgroundColor = 'red'
+}
+
+function manutencao15(){
+    const change = document.getElementById("ar-15").style.backgroundColor = 'gold'
+}
+
+//Armario 16
+function livre16(){
+    const change = document.getElementById("ar-16").style.backgroundColor ='green';    
+}
+
+function ocupado16(){
+    const change = document.getElementById("ar-16").style.backgroundColor = 'red'
+}
+
+function manutencao16(){
+    const change = document.getElementById("ar-16").style.backgroundColor = 'gold'
+}
+
+//Armario 17
+function livre17(){
+    const change = document.getElementById("ar-17").style.backgroundColor ='green';    
+}
+
+function ocupado17(){
+    const change = document.getElementById("ar-17").style.backgroundColor = 'red'
+}
+
+function manutencao17(){
+    const change = document.getElementById("ar-17").style.backgroundColor = 'gold'
+}
+
+//Armario 18
+function livre18(){
+    const change = document.getElementById("ar-18").style.backgroundColor ='green';    
+}
+
+function ocupado18(){
+    const change = document.getElementById("ar-18").style.backgroundColor = 'red'
+}
+
+function manutencao18(){
+    const change = document.getElementById("ar-18").style.backgroundColor = 'gold'
+}
+
+//Armario 19
+function livre19(){
+    const change = document.getElementById("ar-19").style.backgroundColor ='green';    
+}
+
+function ocupado19(){
+    const change = document.getElementById("ar-19").style.backgroundColor = 'red'
+}
+
+function manutencao19(){
+    const change = document.getElementById("ar-19").style.backgroundColor = 'gold'
+}
+
+//Armario 20
+function livre20(){
+    const change = document.getElementById("ar-20").style.backgroundColor ='green';    
+}
+
+function ocupado20(){
+    const change = document.getElementById("ar-20").style.backgroundColor = 'red'
+}
+
+function manutencao20(){
+    const change = document.getElementById("ar-20").style.backgroundColor = 'gold'
+}
